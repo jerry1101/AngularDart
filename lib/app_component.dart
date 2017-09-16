ac78dd33290117ab488b9737410a7a32c3e38a3b
jhung <jerry110160@gmail.com>
2017-09-16 13:56:00 -0400

    Add Hero list

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -15,19 +15,33 @@ class Hero {
 
 @Component(
   selector: 'my-app',
-  directives: const [formDirectives],
+  directives: const [CORE_DIRECTIVES, formDirectives],
+  styleUrls: const ['app_component.css'],
   template: '''
   <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-  <label>name: </label>
-  <input [(ngModel)]="hero.name" placeholder="name">
-</div>
-''',
+<h2>My Heroes</h2>
+<ul class="heroes">
+  <li *ngFor="let hero of heroes">
+    <span class="badge">{{hero.id}}</span> {{hero.name}}
+  </li>
+</ul>
 
+''',
 )
 class AppComponent {
   final title = 'Tour of Heroes';
   Hero hero = new Hero(1, 'Windstorm');
+
+  final List<Hero> heroes = <Hero>[
+    new Hero(11, 'Mr. Nice'),
+    new Hero(12, 'Narco'),
+    new Hero(13, 'Bombasto'),
+    new Hero(14, 'Celeritas'),
+    new Hero(15, 'Magneta'),
+    new Hero(16, 'RubberMan'),
+    new Hero(17, 'Dynama'),
+    new Hero(18, 'Dr IQ'),
+    new Hero(19, 'Magma'),
+    new Hero(20, 'Tornado')
+  ];
 }
