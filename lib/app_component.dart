8239c9f12ffc0cef9d39cf52618310b41d92a268
jhung <jerry110160@gmail.com>
2017-09-16 13:38:37 -0400

    two-way binding for hero.name

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,6 +2,7 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -14,16 +15,19 @@ class Hero {
 
 @Component(
   selector: 'my-app',
+  directives: const [formDirectives],
   template: '''
   <h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label>{{hero.name}}</div>
+  <div>
+  <label>name: </label>
+  <input [(ngModel)]="hero.name" placeholder="name">
+</div>
 ''',
 
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-
   Hero hero = new Hero(1, 'Windstorm');
 }
