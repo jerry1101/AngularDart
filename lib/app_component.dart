8f4e0590f6543a4a17329048b795885ef5e29a5d
jhung <jerry110160@gmail.com>
2017-09-15 23:14:21 -0400

    Add Hero class Use attribute of Hero

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,15 +3,21 @@
 
 import 'package:angular/angular.dart';
 
-
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
 
 @Component(
-    selector: 'my-app',
-    template: '<h1>Hello {{name}}</h1>',
+  selector: 'my-app',
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
 )
 class AppComponent {
+  final title = 'Tour of Heroes';
 
-  var name = 'Angular';
+  Hero hero = new Hero(1, 'Windstorm');
 }
